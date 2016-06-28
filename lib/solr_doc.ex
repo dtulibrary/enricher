3bd11ef57eb8eea3e90096028a25801b86998447
Ronan McHugh <mchugh.r@gmail.com>
2016-06-28 13:41:58 +0200

    add holdings parsing data to solr doc

--- lib/solr_doc.ex
+++ lib/solr_doc.ex
@@ -4,7 +4,7 @@ defmodule SolrDoc do
     :author_ts, :title_ts, :journal_title_ts,  :issn_ss,
     :pub_date_tis, :journal_vol_ssf, :journal_page_ssf,
     :doi_ss, :publisher_ts, :publication_place_ts,
-    :cluster_id_ss, :format
+    :cluster_id_ss, :format, :holdings_ssf
   ]
   use ExConstructor
 
@@ -40,13 +40,13 @@ defmodule SolrDoc do
   }
 
   def data(sd) do
-    # take first vals from multivalued fields 
+    # take first vals from multivalued fields
     transform = fn
-      {x, [top | tail]} -> {x, top}
+      {x, [top | _]} -> {x, top}
       {x, y} -> {x, y}
     end
     Map.from_struct(sd)
-    |> Enum.reject(fn {x, y} -> is_nil(y) end)
+    |> Enum.reject(fn {_, y} -> is_nil(y) end)
     |> Enum.into(Map.new, &transform.(&1))
   end
 
@@ -62,5 +62,47 @@ defmodule SolrDoc do
   @doc "Returns the correct field mapping for the given document's format"
   def field_map(sd) do
     Map.get(@field_mappings, sd.format)
+  end
+
+  def journal_holdings(solr_doc) do
+    {:ok, hld} = Poison.decode(solr_doc.holdings_ssf)
+    holdings = SolrDoc.Holdings.new(hld)
+    [
+      from: SolrDoc.Holdings.from(holdings),
+      to: SolrDoc.Holdings.to(holdings),
+      embargo: SolrDoc.Holdings.embargo(holdings)
+    ]
+  end
+
+  defmodule Holdings do
+    defstruct [:fromyear, :fromvolume, :fromissue, :toyear, :tovolume, :toissue, :embargo]
+    use ExConstructor
+
+    def from(holdings) do
+      [holdings.fromyear, holdings.fromvolume, holdings.fromissue]
+      |> holdings_tuple
+    end
+
+    def to(holdings) do
+      [holdings.toyear, holdings.tovolume, holdings.toissue]
+      |> holdings_tuple
+    end
+
+    def embargo(holdings) do
+      case holdings.embargo do
+        nil -> 0
+        x -> x
+      end
+    end
+
+    defp holdings_tuple(data_enum) do
+      nils_to_strings = fn
+        nil ->  ""
+        x -> x
+      end
+      data_enum
+      |> Enum.map(&nils_to_strings.(&1))
+      |> Enum.reduce({}, fn(x, acc) -> Tuple.append(acc, x) end)
+    end
   end
 end
--- test/solr_doc_test.exs
+++ test/solr_doc_test.exs
@@ -52,9 +52,29 @@ defmodule SolrDocTest do
       "timestamp"=>"2016-03-22T11:48:30.289Z",
       "score"=>17.687468
     }
+    solr_journal = %{
+       "_version_" => 1529502580976123905,
+       "access_ss" => ["dtupub", "dtu"],
+       "affiliation_associations_json" => "{\"editor\":[],\"supervisor\":[],\"author\":[]}",
+       "alert_timestamp_dt" => "2012-11-09T14:17:55.467Z",
+       "alis_key_ssf" => ["000132324"], "cluster_id_ss" => ["320735441"],
+       "format" => "journal", "fulltext_availability_ss" => ["UNDETERMINED"],
+       "holdings_ssf" => ["{\"tovolume\":\"62\",\"fromyear\":\"1947\",\"fromvolume\":\"42\",\"toyear\":\"1967\",\"alis_key\":\"000132324\",\"type\":\"printed\"}"],
+       "id" => "1392610167", "issn_ss" => ["03702634"],
+       "journal_title_facet" => ["Power and works engineering"],
+       "journal_title_ts" => ["Power and works engineering"],
+       "member_id_ss" => ["409670022", "320735441"], "score" => 17.585644,
+       "source_ext_ss" => ["ds1:ds1_jnl", "ds1:jnl_alis"],
+       "source_id_ss" => ["ds1_jnl:582772", "jnl_alis:000132324"],
+       "source_ss" => ["ds1_jnl", "jnl_alis"], "source_type_ss" => ["other"],
+       "timestamp" => "2016-03-22T11:48:48.353Z",
+       "title_ts" => ["Power and works engineering"],
+       "udc_ss" => ["621.3", "621", "620.4", "(410)", "(05)"],
+       "update_timestamp_dt" => "2014-03-08T19:48:57.715Z"}
     article = SolrDoc.new(solr_article)
     book = SolrDoc.new(solr_book)
-    {:ok, article: article, book: book}
+    journal = SolrDoc.new(solr_journal)
+    {:ok, article: article, book: book, journal: journal}
   end
 
   test "constructor", %{article: sd} do
@@ -92,6 +112,17 @@ defmodule SolrDocTest do
   test "to_open_url_query", %{article: sd} do
      q = SolrDoc.to_open_url_query(sd)
      assert String.contains?(q, "rft.au=Kenig%2C+Carlos+E.")
+  end
+
+  test "journal", %{journal: journal} do
+    details = [from: {"1947", "42", ""}, to: {"1967", "62", ""}, embargo: 0]
+    assert SolrDoc.journal_holdings(journal) == details
+
+    # try with some other values
+    different_holdings = SolrDoc.new(%{"holdings_ssf": [
+      "{\"placement\":\"Rijkswaterstaat communications\",\"fromissue\":\"1\",\"fromyear\":\"1959\",\"toyear\":\"1991\",\"alis_key\":\"000131534\",\"type\":\"printed\",\"toissue\":\"49\"}"
+      ]})
+    assert SolrDoc.journal_holdings(different_holdings) ==  [from: {"1959", "", "1"}, to: {"1991", "", "49"}, embargo: 0]
   end
 
 end
