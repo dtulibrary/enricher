1789fab9200530b1f0092184ab1ce43fbb0fa1ac
Ronan McHugh <mchugh.r@gmail.com>
2016-07-12 13:55:07 +0200

    Code cleanup - unnecessary files

--- lib/query_getit.ex
+++
@@ -1,5 +0,0 @@
-defmodule QueryGetit do
-  def url do
-    Application.get_env(:enricher, :getit_url) 
-  end
-end
--- test/query_getit_test.exs
+++
@@ -1,7 +0,0 @@
-defmodule QueryGetitTest do
-  use ExUnit.Case
-
-  test "url" do
-    assert QueryGetit.url == "http://localhost:3000?resolve" 
-  end
-end
