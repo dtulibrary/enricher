ca9188e250b3e2e6aa04f9775313a07d689b79f8
Ronan McHugh <mchugh.r@gmail.com>
2016-08-02 16:13:31 +0200

    remove unused methods

--- lib/enricher.ex
+++ lib/enricher.ex
@@ -15,12 +15,4 @@ defmodule Enricher do
     opts = [strategy: :one_for_one]
     {:ok, _pid} = Supervisor.start_link(children, opts)
   end
-
-  def run do
-    {:ok, input_stack} = Stack.start_link
-    {:ok, update_stack} = Stack.start_link
-    MetastoreMaster.add_articles_to_stack(input_stack)
-    AccessDecider.process(input_stack, update_stack)
-    MetastoreUpdater.run(update_stack)
-  end
 end
--- lib/metastore_master.ex
+++ lib/metastore_master.ex
@@ -1,10 +1,5 @@
 defmodule MetastoreMaster do
   def add_articles_to_stack(stack_pid) do
-
     SolrClient.stack_all_articles(stack_pid)
-  end
-
-  def process_documents(stack_pid) do
-
   end
 end
