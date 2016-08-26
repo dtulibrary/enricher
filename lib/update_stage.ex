ae8ec479c0f3a99b48a027601ce34f04abc414e3
Ronan McHugh <mchugh.r@gmail.com>
2016-08-26 14:20:44 +0200

    Do commits after every million updates

--- lib/update_stage.ex
+++ lib/update_stage.ex
@@ -3,19 +3,22 @@ defmodule UpdateStage do
   use GenStage
   require Logger
 
+  @buffer_size 1000000
   def init(:ok) do
-    {:consumer, :ok}
+    {:consumer, 0}
   end
 
-  def handle_events(updates, _from, :ok) do
+  def handle_events(updates, _from, update_count) do
     Logger.info "handling update events"
     MetastoreUpdater.update_docs(updates)
-    MetastoreUpdater.commit_updates
-    {:noreply, [], :ok}
+    new_count = commit_buffer(updates, update_count)
+    {:noreply, [], new_count}
   end
   
   def handle_info({{prev, _sub}, :nomoredocs}, _state) do
-    Logger.info "Received message :nomoredocs"
+    Logger.info "Received message :nomoredocs - committing and exiting..."
+    MetastoreUpdater.commit_updates
+    :timer.sleep(60000)
     GenStage.stop(prev, :nomoredocs)
     GenStage.stop(self, :nomoredocs)
   end
@@ -24,5 +27,20 @@ defmodule UpdateStage do
     Logger.info "Commit succeeded"
     {:noreply, [], :ok}
   end
+
   def handle_info(_generic, :ok), do: {:noreply, [], :ok}
+
+  @doc """
+  Manage update buffer - if over buffer size - commit
+  Return new buffer size
+  """
+  def commit_buffer(events, update_count) do
+    count = Enum.count(events) + update_count 
+    if count >= @buffer_size do
+      Logger.info "Buffer is full - committing #{count} updates..."
+      MetastoreUpdater.commit_updates
+      0
+    else count
+    end
+  end
 end
